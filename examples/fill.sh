6dcffe1291fe023295d2d30afaad3ac80114d850
Vladimir Redkin <vsredkin01@gmail.com>
2023-10-08 01:14:51 +0300

    add scripts for easier example testing

---
+++ examples/fill.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm -f examples/file.txt examples/file.txt.1 examples/registry
+printf "first
+second
+third
+" > examples/file.txt
---
+++ examples/rotate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mv examples/file.txt examples/file.txt.1
+echo 'fourth' > examples/file.txt
